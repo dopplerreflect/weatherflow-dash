cd33daa24e554106bdf361ef83104284c7b70d10
David Rose <doppler@gmail.com>
2020-09-15 09:28:29 -0400

    more deno 1.4.0 fixes

--- deno-server/deps.ts
+++ deno-server/deps.ts
@@ -8,4 +8,4 @@ export {
 export { v4 } from "https://deno.land/std/uuid/mod.ts";
 export * as flags from "https://deno.land/std/flags/mod.ts";
 export * as path from "https://deno.land/std/path/mod.ts";
-export { Client } from "https://raw.githubusercontent.com/deno-postgres/deno-postgres/master/mod.ts";
+export { Client } from "https://raw.githubusercontent.com/doppler/deno-postgres/fix-for-deno-1.4.0/mod.ts";
