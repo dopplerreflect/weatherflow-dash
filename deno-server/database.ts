c656c36d6b62a90f32fd1b2c35d054be3e09ff72
David Rose <doppler@gmail.com>
2021-02-01 21:52:59 -0500

    switch to https://deno.land/x/postgres/mod.ts

--- deno-server/database.ts
+++ deno-server/database.ts
@@ -1,24 +1,28 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { Client } from "https://raw.githubusercontent.com/doppler/deno-postgres/fix-for-deno-1.4.0/mod.ts";
+// import { Client } from "https://raw.githubusercontent.com/doppler/deno-postgres/fix-for-deno-1.4.0/mod.ts";
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+import type { WfData } from "./types.d.ts";
 
 const ENV = config();
 
 const client = new Client(
   ENV.DEVELOPMENT
-    ? "postgres://app:app@localhost"
+    ? "postgres://app:app@localhost/app"
     : Deno.env.toObject()["DATABASE_URL"],
 );
 
 await client.connect();
 
 export const getCache = async () => {
-  const result = await client.query("select * from wf_cache where id=1");
-  const [id, data] = result.rows[0];
+  const result = await client.queryArray<[WfData]>(
+    "select data from wf_cache where id=1",
+  );
+  const [data] = result.rows[0];
   return data;
 };
 
 export const setCache = async (data: string) => {
-  const result = await client.query(
+  const result = await client.queryArray(
     "update wf_cache set data=$1 where id=1;",
     data,
   );
